c1af15274950d6c8afa8b2d7bd73364a156ca315
Anushree Agrawal <anushree.agrawal27@gmail.com>
2016-10-22 13:58:53 -0500

    Edit for meterological data

--- convert.rb
+++ convert.rb
@@ -7,10 +7,10 @@ data = JSON.parse(IO.read(ARGV[0]))
 coordinates = []
 scale_factor = 0.0000007
 
-data['rows'].each do |row|
-  coordinates << row["latitude"].to_f << row["longitude"].to_f << (row["downloads"] * scale_factor).to_f
+data.each do |row|
+  coordinates << row["latitude"].to_f << row["longitude"].to_f << (row["mass"] * scale_factor).to_f
 end
 
-File.open 'downloads.json', 'w' do |f|
+File.open 'mass.json', 'w' do |f|
   f.write [['2013', coordinates]].to_json
 end
